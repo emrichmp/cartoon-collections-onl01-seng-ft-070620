25d195a6aff5b466cddc61d74726c55e79688a52
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-10 20:15:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,11 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.each do |call|
+    if call.length
+  end
 end
 
 def find_the_cheese# code an argument here

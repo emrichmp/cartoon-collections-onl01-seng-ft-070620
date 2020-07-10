c0cad515371ef00b34e00bca6fa9c30fdb77850c
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-10 20:06:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf_array)# code an argument here
   # Your code here
+  dwarf_arrray.each_with_index do |dwarf, index|
+    puts "#{index} #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here

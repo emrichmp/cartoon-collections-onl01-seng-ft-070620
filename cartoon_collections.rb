d17146f38a66c938b2ba3777eae4d7b28da45b71
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-10 20:29:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,4 +24,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include? do |food|
+    
+  end
 end

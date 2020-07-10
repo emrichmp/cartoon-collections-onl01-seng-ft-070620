f532a20ba5daa107469365a51f4cfa4737c9a79c
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-10 20:06:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(dwarf_array)# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
-  dwarf_arrray.each_with_index do |dwarf, index|
+  array.each_with_index do |dwarf, index|
     puts "#{index} #{dwarf}"
   end
 end

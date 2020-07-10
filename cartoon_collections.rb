0a857a392c19ce44e8b6c3ad19a2d738358bbbcc
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-10 20:22:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.each do |call|
+  array.include? do |call|
     if call.length > 4
       return true
     else
